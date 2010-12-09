5f421c79e03db0869c02da8b81a8a40c158d69f5
Igor Afonov <afonov@gmail.com>
2010-12-09 18:00:03 +0200

    Simple tokens.

--- lexer.js
+++ lexer.js
@@ -1,5 +1,50 @@
 (function(){
-  this.Translator = {};
+  var Tag = { NUM: 256, ID: 257, TRUE: 258, FALSE: 259 };
+
+  var token = function(spec) {
+    var that = {};
+
+    that.getTag = function() {
+      return spec.tag;
+    };
+
+    that.toString = function() {
+      return "<tag: " + spec.tag + ">";
+    };
+
+    return that;
+  };
+
+  var number = function(spec) {
+    var that = token({tag: Tag.NUM});
+
+    that.getNumber = function() {
+      return spec.value;
+    };
+
+    return that;
+  };
+
+  var word = function(spec) {
+    var that = token({tag: spec.tag});
+
+    that.getLexeme = function() {
+      return spec.lexeme;
+    };
+
+    return that;
+  };
+
+  var Words = {};
+  var reserve = function(keywords) {
+    var tagByKeyWord = function(keyword) {
+      return; // Placeholder
+    };
+
+    _(keywords).each(function(keyword) { Words.push(word({lexeme: keyword, tag: TAG_BY_KEYWORD})); });
+  };
+
+  reserve("if", "else");
 
   this.Lexer = function(text) {
     this.text = text;
@@ -18,29 +63,42 @@
           this.pos++;
         } while (isDigit(this.peek()));
 
-        token = v;
+        var t = number({value: v});
         this.pos++;
-        return token;
+        return t;
       }
 
-      var token = this.peek();
-      this.pos++;
-      return token;
+      if (isAlpha(this.peek())) {
+        var str = "";
+        do {
+          str += this.peekAndAdvance();
+        } while (isAlpha(this.peek()) || isDigit(this.peek()));
+
+        return word({tag: Tag.ID, lexeme: str});
+      }
+
+      if (this.peek() !== "") {
+        return token({tag: this.peekAndAdvance()});
+      }
+
+      // EOF
     },
 
     getTokensList: function() {
       var tokens = [];
-      while (token = this.getNextToken()) { tokens.push(token); }
+      while (t = this.getNextToken()) { tokens.push(t); }
       return tokens;
+    },
+
+    peekAndAdvance: function() {
+      var peek = this.peek();
+      this.pos++;
+      return peek;
     },
 
     peek: function() {
       return this.text.charAt(this.pos);
     }
   });
-
-  function isDigit(ch) {
-     return (ch >= '0' && ch <= '9');
-  };
 })();
 
